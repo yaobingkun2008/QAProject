13fd049e96f59d90c340122b4fd9adcbbff796d1
huoniao <yaobingkun2008@126.com>
2016-10-17 15:45:51 +0800

    增加了登陆的后台代码（初步的），增加了一个无关的测试文件

--- DynamicQAProject/src/action/register.java
+++ DynamicQAProject/src/action/register.java
@@ -8,9 +8,114 @@ public class register{//这个类用于截取注册的信息
 	private String email;
 	private String userid;
 	private String friends;
+	private String confirmpassword;
 	
-	public void insert_user(){
-		Connection a = new initialize().getlink();//调用函数进行初始化
+	public String insert_user()
+	{
+		if(this.name.length()!=0 && this.email.length()!=0 && this.password.length()!=0)
+		{
+			if(this.password.equals(this.confirmpassword)==false)
+			{
+				return "passworderror";
+			}
+		}
+		else
+		{
+			return "emptyerror";
+		}
+		/*下面验证是否注册名重复*/
+		Connection a1 = new initialize().getlink();//调用函数进行初始化
+		boolean isrepeat = false;
+		String sql1 = "select * from user where Name=\""+this.name+"\"";
+		try
+		{
+			Statement stmt1 = a1.createStatement();
+			ResultSet rs1 = stmt1.executeQuery(sql1);
+			while(rs1.next()!=false)
+			{
+				isrepeat = true;
+			}
+		}
+		catch (SQLException e1)
+		{
+			e1.printStackTrace();
+		}
+		if(isrepeat == true)
+		{
+			return "nameexist";
+		}
+		else//没有重复，可以插入
+		{
+			/*先得到当前的编号 并更新当前的编号*/
+			int idnum = 0;
+			try
+			{
+				String sql2 = "select * from nowinformation";
+				String sql3 = "";
+				String s1 = "";
+				Statement stmt2 = a1.createStatement();
+				ResultSet rs2 = stmt2.executeQuery(sql2);
+				while(rs2.next()!=false)
+				{
+					idnum = Integer.parseInt(rs2.getString(1))+1;
+					s1 = rs2.getString(1);
+				}
+				sql3 = "update nowinformation set idnow="+"\""+String.valueOf(idnum)+"\""+"where idnow="+"\""+s1+"\"";                
+				stmt2.executeUpdate(sql3);
+			}
+			catch (SQLException e2)
+			{
+				e2.printStackTrace();
+			}
+			/*得到了类型为int的编号*/
+			try
+	        {
+	        	String sql4="insert into user(Name,PassWord,Email,UserID) values(?,?,?,?)";
+	            PreparedStatement ps=a1.prepareStatement(sql4);
+	            ps.setString(1, this.name);
+	            ps.setString(2, this.password);
+	            ps.setString(3, this.email);
+	            ps.setString(4,	String.valueOf(idnum));
+	        }
+	        catch(Exception e3)
+	        {
+	        	e3.printStackTrace();
+	        }
+			return "insertsuccess";//插入成功了
+		}
+		/*这样就插入成功了*/
 	}
 	
+	public void setname(String name)
+	{
+		this.name = name;
+	}
+	public void setemail(String email)
+	{
+		this.email = email;
+	}
+	public void setpassword(String password)
+	{
+		this.password = password;
+	}
+	public void setconfirmpassword(String confirmpassword)
+	{
+		this.confirmpassword = confirmpassword;
+	}
+	public String getname()
+	{
+		return this.name;
+	}
+	public String getemail()
+	{
+		return this.email;
+	}
+	public String getpassword()
+	{
+		return this.password;
+	}
+	public String getconfirmpassword()
+	{
+		return this.confirmpassword;
+	}
 }
---
+++ DynamicQAProject/src/action/testmysql.java
@@ -0,0 +1,63 @@
+package action;
+
+import java.sql.*;
+
+public class testmysql 
+{
+	/*
+	public static void InsertSql()
+	{
+        try
+        {
+        	String insql="insert into user(Name,PassWord,Email,UserID) values(?,?,?,?)";
+        	Connection conn = new initialize().getlink();
+            PreparedStatement ps=conn.prepareStatement(insql);
+            ps.setString(1, "222");
+            ps.setString(2, "222");
+            ps.setString(3, "222");
+            ps.setString(4,"222");
+            int result=ps.executeUpdate();
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
+    }
+	public static void main(String[] args)
+	{
+		InsertSql();
+	}
+	*/
+	/*
+	public static void testfixdata()
+	{
+		int idnum = 0;
+		try
+		{
+			Connection a1 = new initialize().getlink();
+			String sql2 = "select * from nowinformation";
+			String sql3 = "";
+			String s1 = "";
+			Statement stmt2 = a1.createStatement();
+			ResultSet rs2 = stmt2.executeQuery(sql2);
+			while(rs2.next()!=false)
+			{
+				idnum = Integer.parseInt(rs2.getString(1))+1;
+				s1 = rs2.getString(1);
+				System.out.println(s1);
+			}
+			sql3 = "update nowinformation set idnow="+"\""+String.valueOf(idnum)+"\""+"where idnow="+"\""+s1+"\"";                
+			stmt2.executeUpdate(sql3);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println(idnum);
+	}
+	*/
+	public static void main(String[] args)
+	{
+		//testfixdata();
+	}
+}
